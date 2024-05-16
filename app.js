20767544b54625f4838b00ad1ff173562bb2fb86
GABRIEL DE SOUSA <gabrielsousa098121@gmail.com>
2024-05-16 11:14:58 -0300

    iniciando api

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
+const usuario = [
+    { nome: "junior", idade: 20, peso: 50 },
+    { nome: "hugo", idade: 30 },
+    { nome: "victor", idade: 18, peso: 75, cep: "77807060" }
+];
 
-    res.send("minha api")
+app.use("/todos", (req, res, next) => {
+    res.send(usuario);
+});
 
-})
+app.use("/nome", (req, res, next) => {
+    res.send(usuario[1].nome);
+});
 
+app.use("/cep", (req, res, next) => {
+    res.send(usuario[2].cep);
+});
 
-module.exports = app
+app.use("/soma", (req, res, next) => {
+    
+    let somaIdades = 0;
+    usuario.forEach(user => {
+        if (user.idade) {
+            somaIdades += parseInt(user.idade);
+        }
+    });
+    res.send(`A soma das idades é: ${somaIdades}`);
+});
+
+module.exports = app;
